ee7a4b56b04f2d1ec021453e7842759c38db3e9b
gregor <gregor.aubrey.uk@gmail.com>
2023-08-29 17:25:45 +0100

    made app.get function

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json({
+        message: 'Get all contacts'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running and listening on port ${port}.`);
 })
